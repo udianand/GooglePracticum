d8c6eb8004246e8161f2e2e28b24470195a977fd
Siddharth Bhaduri <sbhadur2@illinois.edu>
2017-09-26 16:38:16 -0500

    Data Pre-Processing Code

---
+++ Data/Maize/data_preprocessing_template.py
@@ -0,0 +1,41 @@
+# Google Practicum
+
+# Data Preprocessing
+
+# Importing Libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+    
+# Importing dataset
+
+Dataset = pd.read_csv('Data.csv')
+X = Dataset.iloc[:,:-1].values
+Y = Dataset.iloc[:,3].values
+
+# Taking care of missing data.
+
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values='NaN', strategy = 'mean', axis = 0,)
+imputer = imputer.fit(X[:,1:3]) # Not 2 instead of 3 as the uper bound is excluded.
+X[:,1:3] = imputer.transform(X[:,1:3])
+
+# Encoding Categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:,0] = labelencoder_X.fit_transform(X[:,0]) 
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_Y = LabelEncoder()
+Y = labelencoder_Y.fit_transform(Y) 
+
+# Splitting the dataset into test set and training set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size = 0.2, random_state = 0)
+
+# Feature Scalling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
