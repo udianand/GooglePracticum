c361cef32b73d663b9fa804002c85b9e5e18757a
joonhay2 <31570531+joonhay2@users.noreply.github.com>
2017-11-07 23:01:31 -0600

    Add files via upload

---
+++ Model_Conclusion/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  7 16:43:16 2017
+
+@author: joonhay2
+"""
+
+from model_conclusion_class import Model_Conclusion
+
+
+Result = Model_Conclusion()
+Result.Run_models()
+Result.Results()
+Result.Details()
